88278e78776715833daa574e2c7d9a2bca85dce2
Yuting Hu <yuting.hu.careers@gmail.com>
2025-10-27 16:44:37 -0700

    feat(blog): add blog cards list and click event routes (page content empty)

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { blogCategories } from '../assets/assets'
+import { blog_data, blogCategories } from '../assets/assets'
 import { motion } from "motion/react"
+import BlogCard from './BlogCard'
 
 const BlogList = () => {
 
@@ -32,8 +33,10 @@ const BlogList = () => {
                 ))}
             </div>
 
-            <div>
-                {/* blog-card */}
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3. xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
+                {blog_data.filter((blog)=> menu ==="All" ? true : blog.category=== menu).
+                map((blog)=> <BlogCard key={blog._id} blog={blog}></BlogCard>)
+                }
             </div>
         </div>
     )
